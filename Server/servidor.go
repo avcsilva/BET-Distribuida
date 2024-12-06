611f34dc66fff897537a96e1f3e1b5e3bd1afb74
avcsilva <avcsilva@ecomp.uefs.br>
2024-12-06 20:35:13 -0300

    refactor: adiciona esqueleto para token ring
    Co-Authored-By: SivalLeao <102902290+SivalLeao@users.noreply.github.com>

--- Server/servidor.go
+++ Server/servidor.go
@@ -2,26 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+// Endereços de servidores para testes e usos locais
 var serv_loc_A string = "http://localhost:8080"
 var serv_loc_B string = "http://localhost:8081"
 var serv_loc_C string = "http://localhost:8082"
 
+// Endereços de servidores para testes e usos em laboratório
 var serv_lab_A string = "http://172.16.103.14:8080"
 var serv_lab_B string = "http://172.16.103.13:8080"
 var serv_lab_C string = "http://172.16.103.12:8080"
 
+// Variáveis para o uso de Token Ring
+var token bool = false
+var trava_token bool = false
+var tarefa_ok bool = true
+
+// Variáveis para lógica do sistema
+var id_cont_cliente int = 1
+var id_cont_evento int = 1
+
+// Estruturas de dados
+// Estrutura de dados para o Cliente
 type Cliente struct {
 	id   int
 	nome string
-    saldo int
+    saldo float64
 }
 
+// Estrutura de dados para o Evento
 type Evento struct {
     id              int
     ativo           bool
@@ -33,11 +49,93 @@ type Evento struct {
     resultado       string
 }
 
+// Estrutura de dados para as informações locais do servidor em questão
 type Infos_local struct {
 	servidores []string
 	porta      string
+    qual_serv string
 }
 
+// Fim das estruturas de dados
+
+// Funções para o Token Ring
+// Função para validar existência de token no sistema com 3 servidores
+func existe_token(serv_local *Infos_local) {
+    // Verifica se a variável token se torna verdadeira dentro de um tempo limite
+    for i := 0; i < 3; i++ {
+        if token == true {
+            return
+        }
+        // Conta 1 segundo
+        time.Sleep(1 * time.Second)
+    } // Caso não haja token em 3 segundos, o servidor atual gerará um novo token
+
+    if (serv_local.qual_serv == "A") {
+        time.Sleep(100 * time.Millisecond)
+        if token {
+            return
+        }
+        token = true
+    } else if (serv_local.qual_serv == "B") {
+        time.Sleep(300 * time.Millisecond)
+        if token {
+            return
+        }
+        token = true
+    } else if (serv_local.qual_serv == "C") {
+        time.Sleep(600 * time.Millisecond)
+        if token { 
+            return
+        }
+        token = true
+    }
+    return
+}
+
+// Função para enviar o token via HTTP para o próximo servidor
+func envia_req_token(servidor string) bool {
+    resposta, erro := http.Post(servidor + "/token", "application/json", nil)
+    if erro != nil {
+        return false
+    }
+
+    if resposta.StatusCode == 200 {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Função para segurar e passar o token para o próximo servidor
+func passa_token(serv_local *Infos_local) {
+    for{
+        if (token) {
+            trava_token = true
+            for (trava_token  || !tarefa_ok) {
+                time.Sleep(1 * time.Second)
+                trava_token = false
+            }
+
+            token = false
+            
+            for index, servidor := range serv_local.servidores {
+                if (envia_req_token(servidor)) {
+                    fmt.Printf("Token enviado para o servidor %s\n", servidor)
+                    break
+                } else if (index == 0){
+                    fmt.Printf("Erro ao enviar token para o servidor %s\n. Tentando enviar ao próximo.", servidor)
+                } else {
+                    fmt.Printf("Erro ao enviar token para o servidor %s\n", servidor)
+                    fmt.Printf("Token retornado ao servidor %s\n", serv_local.qual_serv)
+                    token = true
+                    break
+                }
+            }
+        }
+    }
+}
+
+// Função para definir as informações locais do servidor
 func define_info() Infos_local{
 	var qual_serv, tipo_serv string
     fmt.Printf("Tipo de servidor (local [LOC] ou laboratório [LAB]): ")
@@ -50,15 +148,15 @@ func define_info() Infos_local{
         if tipo_serv == "LOC"{
             if qual_serv == "A"{
                 servidores := []string{serv_loc_B, serv_loc_C}
-                serv_local := Infos_local{servidores, "8080"}
+                serv_local := Infos_local{servidores, "8080", qual_serv}
                 return serv_local
             } else if qual_serv == "B" {
-                servidores := []string{serv_loc_A, serv_loc_C}
-                serv_local := Infos_local{servidores, "8081"}
+                servidores := []string{serv_loc_C, serv_loc_A}
+                serv_local := Infos_local{servidores, "8081", qual_serv}
                 return serv_local
             } else if qual_serv == "C" {
                 servidores := []string{serv_loc_A, serv_loc_B}
-                serv_local := Infos_local{servidores, "8082"}
+                serv_local := Infos_local{servidores, "8082", qual_serv}
                 return serv_local
             } else {
                 fmt.Printf("Servidor inválido.")
@@ -72,15 +170,15 @@ func define_info() Infos_local{
         } else if tipo_serv == "LAB"{
             if qual_serv == "A"{
                 servidores := []string{serv_lab_B, serv_lab_C}
-                serv_local := Infos_local{servidores, "8080"}
+                serv_local := Infos_local{servidores, "8080", qual_serv}
                 return serv_local
             } else if qual_serv == "B"{
                 servidores := []string{serv_lab_A, serv_lab_C}
-                serv_local := Infos_local{servidores, "8080"}
+                serv_local := Infos_local{servidores, "8080", qual_serv}
                 return serv_local
             } else if qual_serv == "C"{
                 servidores := []string{serv_lab_A, serv_lab_B}
-                serv_local := Infos_local{servidores, "8080"}
+                serv_local := Infos_local{servidores, "8080", qual_serv}
                 return serv_local
             } else {
                 fmt.Printf("Servidor inválido.")
@@ -175,5 +273,5 @@ func calcularPremio() {
 
 
 func main() {
-
+    serv_local := define_info()
 }
